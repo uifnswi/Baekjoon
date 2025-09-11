c6f4d6946f5e5e968d7793dd853d43d1f1d03958
uifnswi <tnwlspearl@gmail.com>
2025-09-11 20:45:57 +0900

    [Bronze IV] Title: 세 수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(void) {
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	if (a >= b && b >= c || c >= b && b >= a)
+		printf("%d", b);
+	else if (b >= a && a >= c || c >= a && a >= b)
+		printf("%d", a);
+	else if (b >= c && c >= a || a >= c && c >= b)
+		printf("%d", c);
+
+
+	return 0;
+}
