113fdce9fbd05a512dcd1e0209a2039eb634f93e
uifnswi <tnwlspearl@gmail.com>
2025-09-12 23:00:46 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    int H;
+    int M;
+    
+    scanf("%d %d",&H,&M);
+    M-=45;
+    if(M<0)
+    {
+        M+=60;
+        H-=1;
+    }
+    if(H<0)
+        H+=24;
+    printf("%d %d",H,M);
+    
+  
+} 
