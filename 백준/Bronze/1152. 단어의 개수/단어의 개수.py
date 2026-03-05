65fd037b9eeddd2d8a408c217ef18c9cb9cacc2d
uifnswi <tnwlspearl@gmail.com>
2026-03-05 10:48:26 +0900

    [Bronze II] Title: 단어의 개수, Time: 244 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.py
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.py
@@ -1,8 +1,8 @@
-s=input().strip()
-
-if s==" ":
-    print(0)
-    
-else:
-    w=s.split()
-    print(len(w))
+s=input()
+s=s.strip()
+cnt=0
+for i in range(len(s)-1):
+    if s[i]!=s[i+1]:
+        if s[i]==' ':
+            cnt+=1
+print(cnt+bool(s))
