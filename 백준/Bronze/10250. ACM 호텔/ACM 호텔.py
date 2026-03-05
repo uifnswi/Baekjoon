6b4efb0c6e50bb6a6d3f8f9e9cb5cf19c9a40434
uifnswi <tnwlspearl@gmail.com>
2026-03-05 10:12:26 +0900

    [Bronze III] Title: ACM 호텔, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,6 +1,14 @@
 t=int(input())
-for _ in range(t):
-    h,w,n=map(int,input().split())
-    f=(n-1)%h+1
-    r=(n-1)//h+1
-    print(f"{f}{r:02}")
+for i in range(t):
+    H,W,N=map(int, input().split())
+    if N%H==0:
+        h=H
+        w=N//H
+    else:
+        h=N%H
+        w=N//H+1
+    if w>=10:
+        print(str(h)+str(w))
+    else:
+        print(str(h)+"0"+str(w))
+    i+=1
