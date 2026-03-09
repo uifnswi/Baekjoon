867de8354f0bbfab4425420a70105851c3c162a7
uifnswi <tnwlspearl@gmail.com>
2026-03-09 09:29:31 +0900

    [Bronze III] Title: 웰컴 키트, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
@@ -1,11 +1,16 @@
 n=int(input())
-s=list(map(int,input().split()))
-t,p=map(int,input().split())
+arr=list(map(int, input().split()))
+t,p=(map(int,input().split()))
 
-sum=0
-for i in s:
-    sum+=i//t
-    if i%t!=0:
-        sum+=1
-print(sum)
-print(n//p,n%p)
+count=0
+
+for i in range(6):
+    if arr[i]==0:
+        count+=0
+    elif arr[i]%t==0:
+        count+=arr[i]//t
+    else:
+        count+=arr[i]//t+1
+
+print(count)
+print(n//p, n%p)
