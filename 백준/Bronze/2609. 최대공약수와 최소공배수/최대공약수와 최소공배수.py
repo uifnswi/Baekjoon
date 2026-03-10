4605f54d994abae51ba475c565c2f86f0308c67a
uifnswi <tnwlspearl@gmail.com>
2026-03-10 18:28:02 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,12 +1,10 @@
 a,b=map(int, input().split())
-
-def gcd(a,b):
-    while b:
-        a,b=b,a%b
-    return a
-
-g=gcd(a,b)
-l=a*b//g
-
-print(g)
-print(l)
+n=0
+for i in range(min(a,b),0,-1):
+    if a%i==0 and b%i==0:
+        print(i)
+        n=i
+        break
+    
+print(int(a*b/n))
+        
